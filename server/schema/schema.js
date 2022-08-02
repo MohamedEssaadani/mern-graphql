04f93f94e8d850832eb22832fc1d4294b5030b7d
Mohamed Es-saadani <essaadani80@gmail.com>
2022-08-02 10:33:04 +0100

    graphql relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,24 @@
 const graphql = require("graphql");
 // const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 //Dummy data
 const books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+];
+
+const authors = [
+  { name: "Marguerite Duras", age: 44, id: "1" },
+  { name: "Sidney Sheldon", age: 42, id: "2" },
+  { name: "Ernest Hemingway", age: 66, id: "3" },
 ];
 
 const BookType = new GraphQLObjectType({
@@ -15,6 +27,21 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return authors.find((author) => author.id === parent.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -27,6 +54,13 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         //get data from db
         return books.find((book) => book.id === args.id);
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.find((author) => author.id === args.id);
       },
     },
   },
